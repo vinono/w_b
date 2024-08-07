9e87bb4bae3e2d70e1961a00b33e3dc48de93a6e
vinono <vince.he@yandex.com>
2024-08-08 07:43:48 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+// import { ThemeProvider } from "@components/theme-provider";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <header>
+        {/* <div
+          className="bg-black w-8 h-8 rounded-full flex items-center justify-center"
+          data-id="1"
+        >
+          <div className="bg-white w-5 h-5 rounded-full" data-id="2"></div>
+        </div> */}
+      </header>
+
+      <div className="bg-white"> B/W</div>
+    </>
+  );
 }
 
-export default App
+export default App;
